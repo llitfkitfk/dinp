403bc312f6c0dbab9316614c30d3db1a7e32d2cb
Paul <paul@wizmacau.com>
2018-03-09 15:05:26 +0800

    update

---
+++ git-alias.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.sh stash
+
+git config --global alias.cmm 'commit -m'
+git config --global user.email "paul@wizmacau.com"
+git config --global user.name "Paul"
